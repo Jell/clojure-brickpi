0a2dcb580c9bc62a84d84d2d2e829ac3e116e67a
Jell <jean-louis@jawaninja.com>
2015-01-11 18:03:13 +0100

    Revert back to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [serial-port "1.1.2"]
                  [lamina "0.5.2"]   ; needed by gloss
                  [potemkin "0.3.4"] ; needed by gloss
