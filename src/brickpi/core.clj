85226187841b8459ce714d55fbd25e002c00b80a
Jell <jean-louis@jawaninja.com>
2018-07-08 18:11:32 +0200

    Latest

--- src/brickpi/core.clj
+++ src/brickpi/core.clj
@@ -1,8 +1,7 @@
 (ns brickpi.core
   (:require
-   [gloss.core :refer :all]
-   [gloss.io :refer :all]
-   [serial-port :refer :all]))
+   [gloss.core :refer [bit-map]]
+   [serial-port :refer [open write]]))
 
 
 (System/setProperty "gnu.io.rxtx.SerialPorts" "/dev/ttyAMA0")
@@ -11,8 +10,11 @@
 
 (def msg-frame
   (bit-map :type 8,
-           :offset1 1,
-           :offset2 1,
+
+           :offset-1 1,
+           :bits-needed-1 5,
+
+           :offset-2 1,
 
            :m1-speed 8
            :m1-direction 1
@@ -22,19 +24,33 @@
            :m2-direction 1
            :m2-running? 1
 
-           :padding 2
+           :padding 3
            ))
 
 (defn doit []
   (->> (encode msg-frame
-               {:type 3
-                :offset1 false
-                :offset2 false
+               {:type 1
+
+                :offset-1 true
+
+                :offset-2 true
+
                 :m1-speed 255
                 :m1-direction true
                 :m1-running? true
+
                 :m2-speed 255
                 :m2-direction true
                 :m2-running? true
+
                 :padding 0})
-       (write port)))
+       contiguous
+       .array
+       vec
+       ;;(write port)
+       ))
+
+(comment
+  ;; this moves forward:
+  (write port (byte-array (map unchecked-byte [1,218,4,3,132,28,50])))
+  )
